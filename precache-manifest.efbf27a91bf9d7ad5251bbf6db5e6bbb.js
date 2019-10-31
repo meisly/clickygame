221e980f5605cca39165c8d4cacc1945346e8b09
Tierra <sr.undershaft@gmail.com>
2019-10-30 21:45:27 -0500

    Updates

---
+++ precache-manifest.efbf27a91bf9d7ad5251bbf6db5e6bbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de24ae6d86e877794c0df8cf42cf651",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "e776c904433bf993715d",
+    "url": "/clickygame/static/css/main.66703ce0.chunk.css"
+  },
+  {
+    "revision": "f189f686c3cfc58dc824",
+    "url": "/clickygame/static/js/2.bd3a9e65.chunk.js"
+  },
+  {
+    "revision": "e776c904433bf993715d",
+    "url": "/clickygame/static/js/main.61a71f1c.chunk.js"
+  },
+  {
+    "revision": "b4ed2c23f69c9b03baae",
+    "url": "/clickygame/static/js/runtime-main.f70fee65.js"
+  }
+]);
